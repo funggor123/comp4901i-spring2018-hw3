81d132db9219e1c06d41cb5d5bc162ae15348fd8
s31b18 <funghiantie@gmail.com>
2019-04-10 00:36:47 +0800

    add files

---
+++ code/model_char_cnn.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CharCNN(nn.Module):
+
+    def __init__(self, args, vocab_size, embedding_matrix=None):
+        super(CharCNN, self).__init__()
+        # TO DO
+        # hint useful function: nn.Embedding(), nn.Dropout(), nn.Linear(), nn.Conv1d() or nn.Conv2d(),
+
+        self.embedding_dim = int(args.embed_dim)
+
+        self.embedding = nn.Embedding(vocab_size, self.embedding_dim)
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 256, kernel_size=(7, self.embedding_dim), stride=1),
+            nn.ReLU(),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.MaxPool1d(kernel_size=3, stride=3),
+            nn.Conv1d(256, 256, kernel_size=7, stride=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=3, stride=3)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.conv5 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        self.conv6 = nn.Sequential(
+            nn.Conv1d(256, 256, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=3, stride=3)
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(5120, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=int(args.dropout))
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Dropout(p=int(args.dropout))
+        )
+
+        self.fc3 = nn.Linear(1024, int(args.class_num))
+
+    def forward(self, x):
+        # TO DO
+        # input x dim: (batch_size, max_seq_len, D)
+        # output logit dim: (batch_size, num_classes)
+
+        ebd = self.embedding(x)
+        ebd = ebd.unsqueeze(1)
+        out = self.conv1(ebd)
+        out = out.squeeze(3)
+
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.conv5(out)
+        out = self.conv6(out)
+
+        # collapse
+        x = out.view(out.size(0), -1)
+
+        # linear layer
+        x = self.fc1(x)
+        # linear layer
+        x = self.fc2(x)
+        # linear layer
+        x = self.fc3(x)
+
+        return x
---
+++ code/preprocess_char_cnn.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import unicodedata
+import numpy as np
+import torch
+import torch.utils.data as data
+
+PAD_INDEX = 0
+UNK_INDEX = 1
+
+class Vocab():
+    def __init__(self):
+        alphabet = " abcdefghijklmnopqrstuvwxyz0123456789,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+        self.char_dict = {"UNK": UNK_INDEX}
+        self.default_words = 1
+        self.no_vocab = 1
+        for i, char in enumerate(alphabet):
+            self.char_dict[char] = self.default_words + i
+            self.no_vocab +=1
+
+
+def clean(string):
+    string = string.strip()
+    string = string.lower()
+    string = unicodedata.normalize('NFKD', string).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    return string
+
+
+def preprocess(filename, max_len=4000, test=False):
+    df = pd.read_csv(filename)
+    id_ = []  # review id
+    rating = []  # rating
+    content = []  # review content
+    sent_len_list = []
+
+    for i in range(len(df)):
+        id_.append(int(df['id'][i]))
+        if not test:
+            rating.append(int(df['rating'][i]))
+        sentence = clean(str(df['content'][i]))
+        sentence = list(sentence)
+        sent_len = len(sentence)
+        sent_len_list.append(sent_len)
+
+        if sent_len > max_len:
+            content.append(sentence[:max_len])
+        else:
+            content.append(sentence + [" "] * (max_len - sent_len))
+
+    # 6. Sentence Length Mean
+    print("avg " +  str(np.array(sent_len_list).mean()))
+
+    # 7. Sentence Length Std
+    print("std " +  str(np.array(sent_len_list).std()))
+    if test:
+        len(id_) == len(content)
+        return (id_, content, None)
+    else:
+        assert len(id_) == len(content) == len(rating)
+        return (id_, content, rating)
+
+
+class Dataset(data.Dataset):
+    """Custom data.Dataset compatible with data.DataLoader."""
+
+    def __init__(self, data, vocab):
+        self.id, self.X, self.y = data
+        self.vocab = vocab
+        self.num_total_seqs = len(self.X)
+        self.id = torch.LongTensor(self.id)
+        if (self.y is not None): self.y = torch.LongTensor(self.y)
+
+    def __getitem__(self, index):
+        """Returns one data pair (source and target)."""
+        ind = self.id[index]
+        X = self.tokenize(self.X[index])
+        if (self.y is not None):
+            y = self.y[index]
+            return torch.LongTensor(X), y, ind
+        else:
+            return torch.LongTensor(X), ind
+
+    def __len__(self):
+        return self.num_total_seqs
+
+    def tokenize(self, sentence):
+        return [self.vocab.char_dict[char] if char in self.vocab.char_dict else UNK_INDEX for char in sentence]
+
+
+def get_dataloaders(batch_size, max_len):
+    vocab = Vocab()
+
+    train_data = preprocess("train.csv", max_len)
+    dev_data = preprocess("dev.csv", max_len)
+    test_data = preprocess("test.csv", max_len, test=True)
+    train = Dataset(train_data, vocab)
+    dev = Dataset(dev_data, vocab)
+    test = Dataset(test_data, vocab)
+
+    data_loader_tr = torch.utils.data.DataLoader(dataset=train,
+                                                 batch_size=batch_size,
+                                                 shuffle=True)
+    data_loader_dev = torch.utils.data.DataLoader(dataset=dev,
+                                                  batch_size=batch_size,
+                                                  shuffle=False)
+    data_loader_test = torch.utils.data.DataLoader(dataset=test,
+                                                   batch_size=batch_size,
+                                                   shuffle=False)
+    return data_loader_tr, data_loader_dev, data_loader_test, vocab.no_vocab
+
+
